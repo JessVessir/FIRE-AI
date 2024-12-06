90cdae176c7a5bb6b11f3c287752259cf4ce8bea
JessVessir <vessir38@gmail.com>
2024-12-06 13:11:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['233557904154', '233557904155']
+global.prems = ['233557904154', '233557904154', '233557904164']
+global.allowed = ['233557904154', '233557904154', '233557904154']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
